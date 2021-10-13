7f1f992e0cf87830a3d790a17ef7c045a2c70b70
joseancxz <joseanjulcapoma@gmail.com>
2021-10-12 20:32:36 -0500

    jose4

--- LP2Soft/Cursos/frmCursosHome.Designer.cs
+++ LP2Soft/Cursos/frmCursosHome.Designer.cs
@@ -32,13 +32,17 @@ namespace LP2Soft.Cursos
             this.btnFavorito = new System.Windows.Forms.Button();
             this.btnMaterial = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btnLP2Descargar = new System.Windows.Forms.Button();
-            this.btnLP2Compartir = new System.Windows.Forms.Button();
-            this.btnLP2AgregarFavorito = new System.Windows.Forms.Button();
+            this.pictureLP2Descargar = new System.Windows.Forms.PictureBox();
+            this.pictureLP2Compartir = new System.Windows.Forms.PictureBox();
             this.btnLP2 = new System.Windows.Forms.Button();
             this.btnCiclo7 = new System.Windows.Forms.Button();
-            this.btnInformacion = new System.Windows.Forms.Button();
+            this.pictureLP2Favorito = new System.Windows.Forms.PictureBox();
+            this.pictureInformativo = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLP2Descargar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLP2Compartir)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLP2Favorito)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureInformativo)).BeginInit();
             this.SuspendLayout();
             // 
             // btnFavorito
@@ -61,9 +65,9 @@ namespace LP2Soft.Cursos
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.btnLP2Descargar);
-            this.panel1.Controls.Add(this.btnLP2Compartir);
-            this.panel1.Controls.Add(this.btnLP2AgregarFavorito);
+            this.panel1.Controls.Add(this.pictureLP2Favorito);
+            this.panel1.Controls.Add(this.pictureLP2Descargar);
+            this.panel1.Controls.Add(this.pictureLP2Compartir);
             this.panel1.Controls.Add(this.btnLP2);
             this.panel1.Controls.Add(this.btnCiclo7);
             this.panel1.Location = new System.Drawing.Point(12, 105);
@@ -71,35 +75,32 @@ namespace LP2Soft.Cursos
             this.panel1.Size = new System.Drawing.Size(611, 216);
             this.panel1.TabIndex = 3;
             // 
-            // btnLP2Descargar
+            // pictureLP2Descargar
             // 
-            this.btnLP2Descargar.Image = global::LP2Soft.Properties.Resources.download_regular_24;
-            this.btnLP2Descargar.Location = new System.Drawing.Point(76, 41);
-            this.btnLP2Descargar.Name = "btnLP2Descargar";
-            this.btnLP2Descargar.Size = new System.Drawing.Size(33, 34);
-            this.btnLP2Descargar.TabIndex = 6;
-            this.btnLP2Descargar.UseVisualStyleBackColor = true;
-            this.btnLP2Descargar.Click += new System.EventHandler(this.btnLP2Descargar_Click);
+            this.pictureLP2Descargar.Image = global::LP2Soft.Properties.Resources.download_regular_24;
+            this.pictureLP2Descargar.Location = new System.Drawing.Point(93, 43);
+            this.pictureLP2Descargar.Name = "pictureLP2Descargar";
+            this.pictureLP2Descargar.Size = new System.Drawing.Size(24, 24);
+            this.pictureLP2Descargar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureLP2Descargar.TabIndex = 6;
+            this.pictureLP2Descargar.TabStop = false;
+            this.pictureLP2Descargar.Click += new System.EventHandler(this.pictureLP2Descargar_Click);
+            this.pictureLP2Descargar.MouseLeave += new System.EventHandler(this.pictureLP2Descargar_MouseLeave);
+            this.pictureLP2Descargar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureLP2Descargar_MouseMove);
             // 
-            // btnLP2Compartir
+            // pictureLP2Compartir
             // 
-            this.btnLP2Compartir.Image = global::LP2Soft.Properties.Resources.share_alt_regular_24;
-            this.btnLP2Compartir.Location = new System.Drawing.Point(110, 41);
-            this.btnLP2Compartir.Name = "btnLP2Compartir";
-            this.btnLP2Compartir.Size = new System.Drawing.Size(33, 34);
-            this.btnLP2Compartir.TabIndex = 4;
-            this.btnLP2Compartir.UseVisualStyleBackColor = true;
-            this.btnLP2Compartir.Click += new System.EventHandler(this.btnLP2Compartir_Click);
-            // 
-            // btnLP2AgregarFavorito
-            // 
-            this.btnLP2AgregarFavorito.Image = global::LP2Soft.Properties.Resources.plus_circle_regular_24;
-            this.btnLP2AgregarFavorito.Location = new System.Drawing.Point(110, 108);
-            this.btnLP2AgregarFavorito.Name = "btnLP2AgregarFavorito";
-            this.btnLP2AgregarFavorito.Size = new System.Drawing.Size(33, 34);
-            this.btnLP2AgregarFavorito.TabIndex = 5;
-            this.btnLP2AgregarFavorito.UseVisualStyleBackColor = true;
-            this.btnLP2AgregarFavorito.Click += new System.EventHandler(this.btnLP2AgregarFavorito_Click);
+            this.pictureLP2Compartir.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureLP2Compartir.Image = global::LP2Soft.Properties.Resources.share_alt_regular_24;
+            this.pictureLP2Compartir.Location = new System.Drawing.Point(117, 43);
+            this.pictureLP2Compartir.Name = "pictureLP2Compartir";
+            this.pictureLP2Compartir.Size = new System.Drawing.Size(24, 24);
+            this.pictureLP2Compartir.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureLP2Compartir.TabIndex = 4;
+            this.pictureLP2Compartir.TabStop = false;
+            this.pictureLP2Compartir.Click += new System.EventHandler(this.pictureLP2Compartir_Click);
+            this.pictureLP2Compartir.MouseLeave += new System.EventHandler(this.pictureLP2Compartir_MouseLeave);
+            this.pictureLP2Compartir.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureLP2Compartir_MouseMove);
             // 
             // btnLP2
             // 
@@ -122,42 +123,64 @@ namespace LP2Soft.Cursos
             this.btnCiclo7.UseVisualStyleBackColor = true;
             this.btnCiclo7.Click += new System.EventHandler(this.btnCiclo7_Click);
             // 
-            // btnInformacion
+            // pictureLP2Favorito
             // 
-            this.btnInformacion.Image = global::LP2Soft.Properties.Resources.error_circle_regular_24;
-            this.btnInformacion.Location = new System.Drawing.Point(164, 13);
-            this.btnInformacion.Name = "btnInformacion";
-            this.btnInformacion.Size = new System.Drawing.Size(33, 34);
-            this.btnInformacion.TabIndex = 1;
-            this.btnInformacion.UseVisualStyleBackColor = true;
-            this.btnInformacion.Click += new System.EventHandler(this.btnInformacion_Click);
+            this.pictureLP2Favorito.Image = global::LP2Soft.Properties.Resources.plus_circle_regular_24;
+            this.pictureLP2Favorito.Location = new System.Drawing.Point(117, 116);
+            this.pictureLP2Favorito.Name = "pictureLP2Favorito";
+            this.pictureLP2Favorito.Size = new System.Drawing.Size(24, 24);
+            this.pictureLP2Favorito.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureLP2Favorito.TabIndex = 4;
+            this.pictureLP2Favorito.TabStop = false;
+            this.pictureLP2Favorito.Click += new System.EventHandler(this.pictureLP2Favorito_Click);
+            this.pictureLP2Favorito.MouseLeave += new System.EventHandler(this.pictureLP2Favorito_MouseLeave);
+            this.pictureLP2Favorito.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureLP2Favorito_MouseMove);
+            // 
+            // pictureInformativo
+            // 
+            this.pictureInformativo.Image = global::LP2Soft.Properties.Resources.error_circle_regular_24;
+            this.pictureInformativo.Location = new System.Drawing.Point(164, 18);
+            this.pictureInformativo.Name = "pictureInformativo";
+            this.pictureInformativo.Size = new System.Drawing.Size(24, 24);
+            this.pictureInformativo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureInformativo.TabIndex = 4;
+            this.pictureInformativo.TabStop = false;
+            this.pictureInformativo.Click += new System.EventHandler(this.pictureInformativo_Click);
+            this.pictureInformativo.MouseLeave += new System.EventHandler(this.pictureInformativo_MouseLeave);
+            this.pictureInformativo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureInformativo_MouseMove);
             // 
             // frmCursosHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(635, 333);
+            this.Controls.Add(this.pictureInformativo);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnMaterial);
-            this.Controls.Add(this.btnInformacion);
             this.Controls.Add(this.btnFavorito);
             this.Name = "frmCursosHome";
             this.Text = "frmCursosHome";
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLP2Descargar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLP2Compartir)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLP2Favorito)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureInformativo)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnFavorito;
-        private System.Windows.Forms.Button btnInformacion;
         private System.Windows.Forms.Button btnMaterial;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button btnLP2Descargar;
-        private System.Windows.Forms.Button btnLP2Compartir;
-        private System.Windows.Forms.Button btnLP2AgregarFavorito;
         private System.Windows.Forms.Button btnLP2;
         private System.Windows.Forms.Button btnCiclo7;
+        private System.Windows.Forms.PictureBox pictureLP2Compartir;
+        private System.Windows.Forms.PictureBox pictureLP2Descargar;
+        private System.Windows.Forms.PictureBox pictureLP2Favorito;
+        private System.Windows.Forms.PictureBox pictureInformativo;
     }
 }
--- LP2Soft/Cursos/frmCursosHome.cs
+++ LP2Soft/Cursos/frmCursosHome.cs
@@ -18,23 +18,7 @@ namespace LP2Soft.Cursos
         {
             InitializeComponent();
         }
-
-        private void btnInformacion_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Puede agregar a favoritos con el botón + " +
-                "así te llegarán las notificaciones si existe nuevo material");
-        }
-
-        private void btnLP2Descargar_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Descargando");
-        }
-
-        private void btnLP2Compartir_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Compartiendo");
-        }
-
+              
         private void btnLP2AgregarFavorito_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Se agrego a favorito");
@@ -53,5 +37,69 @@ namespace LP2Soft.Cursos
             Ciclo7.ShowDialog();
             this.Close();
         }
+
+        private void pictureLP2Compartir_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.pictureLP2Compartir.BackColor = Color.DarkGray;
+        }
+
+        private void pictureLP2Compartir_MouseLeave(object sender, EventArgs e)
+        {
+            this.pictureLP2Compartir.BackColor = Color.Transparent;
+        }
+
+        private void pictureLP2Compartir_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Compartiendo");
+        }
+
+        private void pictureLP2Descargar_MouseLeave(object sender, EventArgs e)
+        {
+            this.pictureLP2Descargar.BackColor = Color.Transparent;
+        }
+
+        private void pictureLP2Descargar_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.pictureLP2Descargar.BackColor = Color.DarkGray;
+        }
+
+        private void pictureLP2Descargar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Descargando");
+        }
+
+        private void pictureLP2Favorito_MouseLeave(object sender, EventArgs e)
+        {
+            this.pictureLP2Favorito.BackColor = Color.Transparent;
+        }
+
+        private void pictureLP2Favorito_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.pictureLP2Favorito.BackColor = Color.DarkGray;
+        }
+
+        private void pictureLP2Favorito_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Se agregó a favoritos");
+        }
+
+        private void pictureInformativo_MouseLeave(object sender, EventArgs e)
+        {
+            this.pictureInformativo.BackColor = Color.Transparent;
+        }
+
+        private void pictureInformativo_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.pictureInformativo.BackColor = Color.DarkGray;
+        }
+
+        private void pictureInformativo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Puede agregar a favoritos con el botón + " +
+                "así te llegarán las notificaciones si existe nuevo material");
+        }
     }
+
+        
+    
 }
