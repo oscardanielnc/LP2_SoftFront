5b03da44ba8aea334db663ad30216fc6e02afd24
Antony Romero <antony.romero@pucp.edu.pe>
2021-10-13 23:05:55 -0500

    Cambios Eliminar

--- LP2Soft/Eventos/frmModificarEvento.Designer.cs
+++ LP2Soft/Eventos/frmModificarEvento.Designer.cs
@@ -48,6 +48,7 @@ namespace LP2Soft.Eventos
             this.lblModificarEvento = new System.Windows.Forms.Label();
             this.btnSubirImagen = new System.Windows.Forms.Button();
             this.pbImagenPublicacion = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pbImagenPublicacion)).BeginInit();
             this.SuspendLayout();
             // 
@@ -223,11 +224,21 @@ namespace LP2Soft.Eventos
             this.pbImagenPublicacion.TabIndex = 52;
             this.pbImagenPublicacion.TabStop = false;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 610);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 67;
+            this.button1.Text = "Eliminar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // frmModificarEvento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(429, 649);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnModificar);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox5);
@@ -276,5 +287,6 @@ namespace LP2Soft.Eventos
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label lblDescripcion;
         private System.Windows.Forms.Label lblModificarEvento;
+        private System.Windows.Forms.Button button1;
     }
 }
