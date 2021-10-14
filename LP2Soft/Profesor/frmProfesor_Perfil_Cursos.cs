23538325e3246f04fbccfa33046b11141167b7e3
SandroHM26 <a20171364@pucp.edu.pe>
2021-10-13 22:42:23 -0500

    Estoy acabando profesores

---
+++ LP2Soft/Profesor/frmProfesor_Perfil_Cursos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LP2Soft.Profesor
+{
+    public partial class frmProfesor_Perfil_Cursos : Form
+    {
+        public frmProfesor_Perfil_Cursos()
+        {
+            InitializeComponent();
+        }
+    }
+}
