1f41adc603e13fea4277e0574b814a0b133df246
oscardanielnc <oscar.navarro@pucp.edu.pe>
2021-11-10 12:09:25 -0500

    fecha.hora formato correcto

---
+++ LP2Soft/CalculadorNotas/regNotas.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace LP2Soft.CalculadorNotas
+{
+    partial class regNotas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.panelNotas = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.BackColor = System.Drawing.SystemColors.Control;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(12, 18);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(120, 16);
+            this.lblNombre.TabIndex = 19;
+            this.lblNombre.Text = "Laboratorio (3) *";
+            // 
+            // panelNotas
+            // 
+            this.panelNotas.AutoSize = true;
+            this.panelNotas.BackColor = System.Drawing.SystemColors.Control;
+            this.panelNotas.Location = new System.Drawing.Point(6, 44);
+            this.panelNotas.Name = "panelNotas";
+            this.panelNotas.Size = new System.Drawing.Size(168, 45);
+            this.panelNotas.TabIndex = 20;
+            // 
+            // regNotas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(180, 104);
+            this.Controls.Add(this.panelNotas);
+            this.Controls.Add(this.lblNombre);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "regNotas";
+            this.Text = "regNotas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Panel panelNotas;
+    }
+}
